7860123559dfcd1524c109ad37fd7f9c3ae0d335
madortill <mador.till@gmail.com>
2026-06-09 17:01:44 +0300

    09/06/26 #7

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,77 @@
 import React from "react";
-import { useState } from "react";
+import { useState , useRef , useEffect } from "react";
 import "./css/App.css";
 import bahad13Logo from "./assets/logo.png";
 import tillLogo from "./assets/till_bluelogo.svg";
 import circlesBottom from "./assets/circles-bottom.svg";
 import circlesTop from "./assets/circlesTop.svg";
+import aboutIcon from "./assets/aboutIcon.svg";
 
 import Card from "./components/Cards/Cards";
 import card1Image from "./assets/card1-2.png";
 import card2Image from "./assets/card2-3.png";
 
 function App() {
+  const [aboutOpen, setAboutOpen] = useState(false);
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setAboutOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       <div className="app">
+        {/* אודות */}
+        <div className="about-container" ref={popupRef}>
+          <img
+            src={aboutIcon}
+            className="about-btn"
+            onClick={() => setAboutOpen(!aboutOpen)}
+          />
+
+          {aboutOpen && (
+            <div className="about-popup">
+              <div className="about-section">
+                <strong>מפתחת ראשית:</strong>
+                {/* <img src={mapal} className="about-dev-img" /> */}
+                <span>טוראי קשת פרי</span>
+              </div>
+
+              <div className="about-section">
+                <strong>גרפיקה:</strong>
+                <span>טוראי קשת פרי</span>
+              </div>
+
+              <div className="about-section">
+                <strong>מומחי תוכן:</strong>
+                <span>סגן עדן רוקח</span>
+                <span>רב"ט שיר אשר לוי</span>
+              </div>
+
+              <div className="about-section">
+                <strong>רמ״ד טי״ל:</strong>
+                <span>רס"מ קטיה מדבדב</span>
+              </div>
+
+              <div className="about-section">
+                <strong>גרסה:</strong>
+                <span>יוני 2026</span>
+              </div>
+            </div>
+          )}
+        </div>
+
         <img src={bahad13Logo} className="logo-bahad13" alt="logo" />
         <img src={tillLogo} className="till-logo" alt="till" />
         <img
@@ -35,7 +93,10 @@ function App() {
             buttonText="להתחלת הלומדה >"
             buttonColor="#992045"
             // onClick={() => console.log("card 1")}
-            onClick={() => window.location.href = "https://madortill.github.io/lomdat-principles/"}
+            onClick={() =>
+              (window.location.href =
+                "https://madortill.github.io/lomdat-principles/")
+            }
           />
 
           <Card
@@ -46,8 +107,10 @@ function App() {
             buttonText="להתחלת הלומדה >"
             buttonColor="#114687"
             // onClick={() => console.log("card 2")}
-            onClick={() => window.location.href = "https://madortill.github.io/lomdat-traffic-reports/"}
-
+            onClick={() =>
+              (window.location.href =
+                "https://madortill.github.io/lomdat-traffic-reports/")
+            }
           />
         </div>
       </div>
